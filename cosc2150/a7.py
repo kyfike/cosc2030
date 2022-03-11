a4b03b5564a9351a9f1b2f62b2d148bc725db172
Ky Fike <kyfike@gmail.com>
2022-03-10 19:46:04 -0700

    My first python program- viewing addresses

---
+++ cosc2150/a7.py
@@ -0,0 +1,18 @@
+intone = 1
+print(intone)
+print(hex(id(intone)))
+inttwo = 2
+print(inttwo)
+print(hex(id(inttwo)))
+intthree = 3
+print(intthree)
+print(hex(id(intthree)))
+intfour = 4
+print(intfour)
+print(hex(id(intfour)))
+intfive = 5
+print(intfive)
+print(hex(id(intfive)))
+intsix = 6
+print(intsix)
+print(hex(id(intsix)))

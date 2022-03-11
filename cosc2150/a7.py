5900d2b2ecc782dd881ee0e841f356d86b636897
Ky Fike <kyfike@gmail.com>
2022-03-10 19:51:08 -0700

    Print int, float, and string addresses

--- cosc2150/a7.py
+++ cosc2150/a7.py
@@ -1,3 +1,4 @@
+print("integers")
 intone = 1
 print(intone)
 print(hex(id(intone)))
@@ -11,6 +12,66 @@ intfour = 4
 print(intfour)
 print(hex(id(intfour)))
 intfive = 5
+print(intfive)
+print(hex(id(intfive)))
+intsix = 6
+print(intsix)
+print(hex(id(intsix)))
+
+print("float")
+intone = float(1)
+print(intone)
+print(hex(id(intone)))
+inttwo = float(2)
+print(inttwo)
+print(hex(id(inttwo)))
+intthree = float(3)
+print(intthree)
+print(hex(id(intthree)))
+intfour = float(4)
+print(intfour)
+print(hex(id(intfour)))
+intfive = float(5)
+print(intfive)
+print(hex(id(intfive)))
+intsix = float(6)
+print(intsix)
+print(hex(id(intsix)))
+
+print("strings")
+intone = "1"
+print(intone)
+print(hex(id(intone)))
+inttwo = "2"
+print(inttwo)
+print(hex(id(inttwo)))
+intthree = "3"
+print(intthree)
+print(hex(id(intthree)))
+intfour = "4"
+print(intfour)
+print(hex(id(intfour)))
+intfive = "5"
+print(intfive)
+print(hex(id(intfive)))
+intsix = "6"
+print(intsix)
+print(hex(id(intsix)))
+
+print("mixed: int-float-st-st-float-int")
+intone = 1
+print(intone)
+print(hex(id(intone)))
+inttwo = float(2)
+print(inttwo)
+print(hex(id(inttwo)))
+intthree = "3"
+print(intthree)
+print(hex(id(intthree)))
+intfour = "4"
+print(intfour)
+print(hex(id(intfour)))
+intfive = float(5)
 print(intfive)
 print(hex(id(intfive)))
 intsix = 6

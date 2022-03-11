318f4fa21178dafabeed531e4e2953af80c91c57
Ky Fike <kyfike@gmail.com>
2022-03-11 09:54:45 -0700

    .8 A-D solved

---
+++ u3-pointers/e08.cpp
@@ -0,0 +1,58 @@
+/******
+ * Ky Fike on Mar 11, 2022
+ * Exercise 8.8-10:
+ * Write statements to perform the specified task.
+ * Assume: unsigned int are stored in four bytes. Starting address of the built-in array is at location 1002500 in memory.
+ * *****/
+
+/* 8.8
+A.  unsigned int values[size] = {2, 4, 6, 8, 10};
+
+B.  unsigned int* vPtr;
+
+C.  for (int i = 0; i < size; i++){
+        cout << values[i] << endl;
+    }
+
+D.  vPtr = &values[0];
+
+E.  
+
+F.  
+
+G.  
+
+H.  
+
+I.  
+
+J.  
+**************/
+
+/* 8.9
+A.  
+
+B.  
+
+C.  
+
+D.  
+
+E.  
+
+F.  
+
+G.  
+
+**************/
+
+/* 8.10
+A.  
+
+B.  
+
+C.  
+
+D.  
+
+**************/

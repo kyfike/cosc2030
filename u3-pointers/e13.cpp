b6eec7db8cf692d88fec5f7f695242c19b10fca6
Ky Fike <kyfike@gmail.com>
2022-03-11 16:16:53 -0700

    Prep formatting

---
+++ u3-pointers/e13.cpp
@@ -0,0 +1,9 @@
+/******
+ * Ky Fike on Mar 11, 2022
+ * Exercise 8.13:
+ * What does the program do?
+ * *****/
+
+/*
+
+*/
---
+++ u3-pointers/e14.cpp
@@ -0,0 +1,9 @@
+/******
+ * Ky Fike on Mar 11, 2022
+ * Exercise 8.14:
+ * What does the program do?
+ * *****/
+
+/*
+
+*/
